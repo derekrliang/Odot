4fcccf8c3e3a666038f41fd5a641b8cda238cc2b
Derek Liang <frostyrusty@gmail.com>
2014-10-01 23:01:32 -0700

    added additional rspec test for login credentials

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -35,7 +35,48 @@ describe UserSessionsController do
         expect(user).to receive(:authenticate)
         post :create, email: "derekliang@gmail.com", password: "derek1"
       end
+
+      it "sets the user_id in the session" do
+        post :create, email: "derekliang@gmail.com", password: "derek1"
+        expect(session[:user_id]).to eq(user.id)
+      end
+
+      it "sets the flash success message" do
+        post :create, email: "derekliang@gmail.com", password: "derek1"
+        expect(flash[:success]).to eq("Thanks for logging in!")
+      end
     end
+  end
+
+  shared_examples_for "denied login" do
+    it "renders the new template" do
+      post :create, email: email, password: password
+      expect(response).to render_template('new')
+    end
+
+    it "sets the flash error message" do
+      post :create, email: email, password: password
+      expect(flash[:error]).to eq("There was a problem logging in. Please check your email and password.")
+    end
+  end
+
+  context "with blank credentials" do
+    let(:email) { "" }
+    let(:password) { "" }
+    it_behaves_like "denied login"
+  end
+
+  context "with an incorrect credentials" do
+    let!(:user) { User.create(first_name: "Derek", last_name: "Liang", email: "derekliang@gmail.com", password: "derek1", password_confirmation: "derek1") }
+    let(:email) { user.email }
+    let(:password) { "incorrect" }
+    it_behaves_like "denied login"
+  end
+
+  context "with no email in existence" do
+    let(:email) { "none@found.com" }
+    let(:password) { "incorrect" }
+    it_behaves_like "denied login"
   end
 
 end
